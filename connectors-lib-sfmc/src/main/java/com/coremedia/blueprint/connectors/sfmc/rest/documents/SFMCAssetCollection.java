33ba3680161cddf580f2a96326c5d22ccb9cdedb
Matthias Faust <matthias.faust@coremedia.com>
2019-01-25 15:39:48 +0100

    qa

--- connectors-lib-sfmc/src/main/java/com/coremedia/blueprint/connectors/sfmc/rest/documents/SFMCAssetCollection.java
+++
@@ -1,7 +0,0 @@
-package com.coremedia.blueprint.connectors.sfmc.rest.documents;
-
-/**
- *
- */
-public class SFMCAssetCollection extends SFMCCollection<SFMCAsset> {
-}
--- connectors-lib-sfmc/src/main/java/com/coremedia/blueprint/connectors/sfmc/rest/documents/SFMCCategoryCollection.java
+++
@@ -1,7 +0,0 @@
-package com.coremedia.blueprint.connectors.sfmc.rest.documents;
-
-/**
- *
- */
-public class SFMCCategoryCollection extends SFMCCollection<SFMCCategory> {
-}
